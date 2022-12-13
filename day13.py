7e261b7542f352da3fb2544948a9aaeacf12a254
Aaron Lael <aaron.j.lael@gmail.com>
2022-12-13 14:40:30 -0700

    removed duplicate function from p2

--- day13.py
+++ day13.py
@@ -89,6 +89,7 @@ print(part1(inp))
 
 
 def part2(inp: str) -> int:
+    """reuses ordercheck() from p1"""
     inp = '\n'.join(inp.split("\n\n")).split("\n") + ["[[2]]"] + ["[[6]]"]
     correct_order_packets = []
     for packet in inp:
@@ -101,7 +102,7 @@ def part2(inp: str) -> int:
                 right = copy.deepcopy(correct_order_packets[i])
                 while True:
                     if len(left) > 0 and len(right) > 0:
-                        left, right, b = order_check2(left, right)
+                        left, right, b = order_check(left, right)
                         # packet before correct_order_packets[i]
                         if b == True:
                             correct_order_packets = correct_order_packets[:i] + [packet] + correct_order_packets[i:]
@@ -123,56 +124,6 @@ def part2(inp: str) -> int:
                 correct_order_packets += [packet]
     return (correct_order_packets.index([[2]]) + 1) * (correct_order_packets.index([[6]]) + 1)
 
-
-def order_check2(left: list, right: list) -> tuple:
-    if len(left) == 0 or len(right) == 0:
-        if len(left) > len(right):
-            return left, right, False
-        elif len(right) > len(left):
-            return left, right, True
-        else:
-            return left, right, None
-    # both ints?
-    if type(left[0]) == I == type(right[0]):
-        l = left.pop(0)
-        r = right.pop(0)
-        if r > l:
-            return left, right, True
-        elif r < l:
-            return left, right, False
-        else:
-            if len(left) > 0 and len(right) > 0 and r == l:
-                return left, right, None
-            elif len(left) == 0 and len(right) > 0:
-
-                return left, right, True
-            elif len(right) == 0 and len(left) > 0:
-                return left, right, False
-            else:
-                return left, right, None
-
-
-    # both lists?
-    elif type(left[0]) == L == type(right[0]):
-        sl = left.pop(0)
-        sr = right.pop(0)
-        while True:
-            sl, sr, order = order_check2(sl, sr)
-            if order in [True, False]:
-                return left, right, order
-            elif len(sl) == 0 and len(sr) > 0:
-                return left, right, True
-            elif len(sr) == 0 and len(sl) > 0:
-                return left, right, False
-            elif len(sr) == 0 == len(sl):
-                return left, right, None
-    # type mismatch correction
-    elif type(left[0]) != type(right[0]):
-        if type(left[0]) != L:
-            left[0] = [left[0]]
-        elif type(right[0]) != L:
-            right[0] = [right[0]]
-        return left, right, None
 
 print("test solution p2")
 print(part2(test))
